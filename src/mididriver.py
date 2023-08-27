21c8c677c66e5f476ed8a44c9ff74f34c4d18ffa
Coolbrother <coolbrother@sourcegod.com>
2023-08-27 04:17:09 +0200

    Adding file: (mididriver.py)

---
+++ src/mididriver.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    File: mididriver.py
+    Module using rtmidi as midi driver
+    Date: Sun, 27/08/2023
+    Author: Coolbrother
+"""
+from rtmidi.midiutil import open_midiport
+class MidiDriver(object):
+    """ Midi driver manager """
+    def __init__(self, ):
+        pass
+
+    #----------------------------------------
+
+#========================================
+
+if __name__ == "__main__":
+    drv = MidiDriver()
+    input("It's Ok...")
+#----------------------------------------
+
